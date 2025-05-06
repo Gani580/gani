c1c8b43af1a5c3b779a673732b0f375816219e72
Gani580 <ganeshng580@gmail.com>
2025-05-06 10:09:34 +0530

    Initial Commit

---
+++ src/main/java/hiworld/App.java
@@ -0,0 +1,13 @@
+package hiworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
